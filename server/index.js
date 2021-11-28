8c90e78473f337350e2e45187798ad14eb3c6e9f
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2021-11-28 22:55:34 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const restaurant = require("./controllers/restaurants");
 const order = require("./controllers/orders");
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`Server is running. Communicate with http://localhost:${port}`);
 });
 exports.app = app;
 // get request that returns "Hello world!"
