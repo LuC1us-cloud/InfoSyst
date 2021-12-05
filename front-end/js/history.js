8d11a81eff6379572529241daa3cb1fd3c3ce6a4
79539642 <patrik.voicechovski@ktu.edu>
2021-12-05 14:14:44 +0200

    user history added

--- front-end/js/history.js
+++ front-end/js/history.js
@@ -25,10 +25,46 @@ const getUserHistory = function(id) {
         },
         success: function (data, _, xhr) {
             if(xhr.status === 200) {
-                console.log(data);
+                renderUserHistory(data);
             }else {
-
+                $('.error__message').html('Įvyko nenumatyta klaidą')
             }
         }
     });
 };
+
+const renderUserHistory = function(orders) {
+    orders.forEach(order => {
+        const {order_status, order_time, order_total_price, restaurant_id} = order;
+        const restaurantName = getRestaurantName(restaurant_id);
+        const date = new Date(order_time);
+        console.log(date.getFullYear());
+        $('.table__body').append(`
+            <tr>
+                <td>${date.getFullYear()}-${date.getMonth()}-${date.getDate()}</td>
+                <td>${restaurantName}</td>
+                <td>${order_total_price}</td>
+                <td>${order_status}</td>
+            </tr>         
+        `);
+    });
+};
+
+const getRestaurantName = function(id) {
+    let restaurantName;
+    $.ajax({
+        type: "get",
+        async: false,
+        url: `http://localhost:3000/getRestaurant/${id}`,
+        data: {
+            "id":id
+        },
+        success: function (data, _, xhr) {
+            if(xhr.status === 200) {
+                restaurantName = data.restaurantName;
+            }
+        }
+    });
+
+    return restaurantName;
+}
